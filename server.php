82720a75e925a92bbd5bdf22ca94bd1fadab1ef2
Fábio Kiatkowski <xolla@xolla.com.br>
2014-08-20 22:36:32 -0300

    Cleanning php file.

--- server.php
+++ server.php
@@ -1,11 +1,10 @@
 <?php
-  
   $cep = $_GET["cep"];
 
   function curl_get_call($url) {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
-    $response = curl_exec($curl); 
+    $response = curl_exec($curl);
     return $response;
   }
 
